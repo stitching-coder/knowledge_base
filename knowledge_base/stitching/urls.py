22662fef2820cdb2c2b9ca6e8a4eee3a2c2a03a8
Laney McGlohon <stitching.coder@gmail.com>
2025-01-26 16:29:40 -0800

    Fix stitching urls

--- knowledge_base/stitching/urls.py
+++ knowledge_base/stitching/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.stitching_entry_list, name='entry_list'),
-    path('entry/<int:pk>/', views.stitching_entry_detail, name='entry_detail'),
+    path('', views.stitching_entry_list, name='stitching_entry_list'),
+    path('entry/<int:pk>/', views.stitching_entry_detail, name='stitching_entry_detail'),
 ]
